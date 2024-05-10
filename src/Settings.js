6b0e38a6b785d26856a4fa1b535aecd545a081e8
Mika Rautio <mika.rautio@gmail.com>
2024-05-10 15:57:20 +0300

    fix default data prefix

--- src/Settings.js
+++ src/Settings.js
@@ -16,7 +16,7 @@ Settings.prototype.getInstance = function () {
 
 Settings.prototype.init = function () {
   this.engine = {
-    demoPathPrefix: 'testdata/',
+    demoPathPrefix: 'data/',
     tool: true,
     fileWatchInterval: 250,
     startDelay: 0, // bump this for demo capturing
