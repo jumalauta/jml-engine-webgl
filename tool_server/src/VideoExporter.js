1addcf9b09317408a3a8cd3beb65c1ee57c4649a
Mika Rautio <1129758+mrautio@users.noreply.github.com>
2025-08-02 11:44:12 +0300

    fix: browser out-of-memory error if capture encoding lags too much
    Improve capture speed and instruct users so that they can avoid browser Out-of-Memory errors

--- tool_server/src/VideoExporter.js
+++ tool_server/src/VideoExporter.js
@@ -6,6 +6,7 @@ const logger = pino();
 
 const VideoExporter = function () {
   this.ready = false;
+  this.fastEncoding = true;
 };
 
 VideoExporter.prototype.setMusicPath = function (path) {
@@ -16,42 +17,93 @@ VideoExporter.prototype.setMusicPath = function (path) {
   this.musicPath = path;
 };
 
+VideoExporter.prototype.setFastEncoding = function (enabled) {
+  this.fastEncoding = enabled;
+};
+
 VideoExporter.prototype.spawn = function (onSpawn, onClose) {
   // * YouTube Recommended upload encoding settings: https://support.google.com/youtube/answer/1722171?hl=en
   // * Audio: AAC-LC audio with high bitrate, stereo/5.1 and samplerate 48/96kHz
   // * Video: 16:9 MP4 H.264 60 fps (/w nearly lossless quality)
 
-  const ffmpeg = spawn('ffmpeg', [
-    // overwrite output file / say yes to everything
-    '-y',
-    // input video
-    '-f',
-    'image2pipe',
-    '-framerate',
-    '60',
-    '-i',
-    '-',
-    // input audio
-    '-i',
-    this.musicPath,
-    '-c:a',
-    'aac',
-    '-b:a',
-    '512k',
-    '-strict',
-    '-2',
-    // output video
-    '-framerate',
-    '60',
-    '-vcodec',
-    'libx264',
-    '-crf',
-    '18',
-    '-shortest',
-    '-filter:v',
-    'scale=1920:-1',
-    'output2.mp4'
-  ]);
+  const outputPath = 'output.mp4';
+  let ffmpegArgs;
+
+  if (this.fastEncoding) {
+    // fast encoding settings - prioritize speed while maintaining quality
+    ffmpegArgs = [
+      // overwrite output file / say yes to everything
+      '-y',
+      // input video
+      '-f',
+      'image2pipe',
+      '-framerate',
+      '60',
+      '-i',
+      '-',
+      // input audio
+      '-i',
+      this.musicPath,
+      '-c:a',
+      'aac',
+      '-b:a',
+      '512k',
+      '-strict',
+      '-2',
+      // output video - fast encoding settings
+      '-framerate',
+      '60',
+      '-vcodec',
+      'libx264',
+      '-preset',
+      'ultrafast',
+      '-tune',
+      'zerolatency',
+      '-crf',
+      '18',
+      '-x264-params',
+      'nal-hrd=cbr',
+      '-shortest',
+      '-filter:v',
+      'scale=1920:-1',
+      outputPath
+    ];
+  } else {
+    // High quality encoding settings (original)
+    ffmpegArgs = [
+      // overwrite output file / say yes to everything
+      '-y',
+      // input video
+      '-f',
+      'image2pipe',
+      '-framerate',
+      '60',
+      '-i',
+      '-',
+      // input audio
+      '-i',
+      this.musicPath,
+      '-c:a',
+      'aac',
+      '-b:a',
+      '512k',
+      '-strict',
+      '-2',
+      // output video
+      '-framerate',
+      '60',
+      '-vcodec',
+      'libx264',
+      '-crf',
+      '18',
+      '-shortest',
+      '-filter:v',
+      'scale=1920:-1',
+      outputPath
+    ];
+  }
+
+  const ffmpeg = spawn('ffmpeg', ffmpegArgs);
 
   ffmpeg.stdout.on('data', (data) => {
     logger.info(`ffmpeg output: ${data}`);
