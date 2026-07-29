07f0b79f514ff367a9a63a41cd145852623c5a95
Mika Rautio <1129758+mrautio@users.noreply.github.com>
2026-07-29 05:46:25 +0000

    feat: create windows-release in release builds

--- release-windows.sh
+++ release-windows.sh
@@ -4,10 +4,16 @@
 
 set -euo pipefail
 
+ENGINE_VERSION="v3.7.0"
+ENGINE_DIR="engine_v3_7_0"
+ENGINE_ZIP="${ENGINE_DIR}_windows.zip"
+ENGINE_URL="https://github.com/jumalauta/jml-engine-webgl/releases/download/${ENGINE_VERSION}/${ENGINE_ZIP}"
+ENGINE_SHA256SUM="f185bf3aa41d773d888ad247bc9291ce2f1ec2978718282a66f8b44807d7defd"
+
 rm -fr dist/
+#this canbe used to check if build works: npx vite preview
 NODE_ENV=exe npx vite build
 
-#this canbe used to check if build works: npx vite preview
 rm -rf dist/data_*
 rm -rf dist/testdata*
 rm -f dist/playlist.js
@@ -17,30 +23,23 @@ CURRENT_DIR=$(pwd)
 TMPDIR=$(mktemp -d)
 echo "Preparing release in $TMPDIR"
 
-cp -r dist/ $TMPDIR
-
 cd $TMPDIR
 
-WEBDEMOEXE_VERSION="015"
-WEBDEMOEXE_SHA256SUM="fa7724add6fa218c702814decb5710767b5ebe1b3818d6069a5ee067376454cc"
+wget "${ENGINE_URL}"
+echo "${ENGINE_SHA256SUM}  ${ENGINE_ZIP}" | sha256sum -c
+unzip -x "${ENGINE_ZIP}"
+rm -fr "${ENGINE_ZIP}"
 
-wget https://github.com/pandrr/WebDemoExe/releases/download/release${WEBDEMOEXE_VERSION}/webdemoexe_${WEBDEMOEXE_VERSION}.zip
-echo "${WEBDEMOEXE_SHA256SUM} webdemoexe_${WEBDEMOEXE_VERSION}.zip" | sha256sum -c
-unzip -x webdemoexe_${WEBDEMOEXE_VERSION}.zip
-rm -fr webdemoexe_${WEBDEMOEXE_VERSION}.zip
+# remove old project contents
+rm -fr "${ENGINE_DIR}/demo"
+mkdir -p "${ENGINE_DIR}/demo"
+mkdir -p "${ENGINE_DIR}/demo/data"
 
-cat > webdemoexe.xml <<EOF
-<config>
-	<title>JML</title>
-</config>
-EOF
-
-mv WebDemoExe.exe demo.exe
-node ${CURRENT_DIR}/change-favicon/index.js demo.exe ${CURRENT_DIR}/change-favicon/favicon.ico
-rm -fr demo
-mv dist demo
+# copy the build output, but never the 'data' directory
+find "${CURRENT_DIR}/dist" -mindepth 1 -maxdepth 1 ! -name data -exec cp -r {} "${ENGINE_DIR}/demo/" \;
 
 cd $CURRENT_DIR
-mv $TMPDIR $CURRENT_DIR/dist/release
+mv "$TMPDIR/${ENGINE_DIR}" $CURRENT_DIR/dist/release-windows
+rm -fr $TMPDIR
 
-echo Release is in dist/release - copy your 'data' directory into dist/release/demo/
+echo Release is in dist/release-windows - copy your 'data' directory contents into dist/release-windows/demo/data/
